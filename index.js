810974b4286f09aa581af0cba94bfe9376d7bc5e
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-05-15 10:09:37 -0300

    add responsive menu

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburg = document.getElementById('hamburg');
+const xSection = document.querySelector('.x-section');
+const menuToggle = document.querySelector('.menu-toggle');
+const menuLinks = document.querySelectorAll('.items-menu-toggle');
+
+function toggleMenu() {
+  menuToggle.classList.toggle('show');
+};
