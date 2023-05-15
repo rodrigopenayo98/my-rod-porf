f03d87ed06e5d4876ebd39849617cd7cef42028e
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-05-15 11:13:59 -0300

    fix linters

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const hamburg = document.getElementById("hamburg");
-const xSection = document.querySelector(".x-section");
-const menuToggle = document.querySelector(".menu-toggle");
-const menuLinks = document.querySelectorAll(".items-menu-toggle");
+const hamburg = document.getElementById('hamburg');
+const xSection = document.querySelector('.x-section');
+const menuToggle = document.querySelector('.menu-toggle');
+const menuLinks = document.querySelectorAll('.items-menu-toggle');
 
 function toggleMenu() {
-  menuToggle.classList.toggle("show");
+  menuToggle.classList.toggle('show');
 }
 
-xSection.addEventListener("click", toggleMenu);
-hamburg.addEventListener("click", toggleMenu);
+xSection.addEventListener('click', toggleMenu);
+hamburg.addEventListener('click', toggleMenu);
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click", toggleMenu);
+  link.addEventListener('click', toggleMenu);
 });
